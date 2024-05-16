cd1f0d09390aa56a0a84eb0be2baca3c3861221b
khyernet <khyernet@vivaldi.net>
2024-05-15 18:47:03 -0800

    hyprland-git -> hyprland

--- install.sh
+++ install.sh
@@ -260,7 +260,7 @@ fi
 
 # Install the correct hyprland version
 echo -e "$CNT - Installing Hyprland, this may take a while..."   
-install_software hyprland-git
+install_software hyprland
 
 echo -e "$CNT - Installing software development tools..."   
 for SOFTWR in ${dev_stage[@]}; do
